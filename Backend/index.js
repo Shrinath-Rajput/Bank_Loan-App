933d82751a28cf7c15acce8b52363d00b901ed04
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-06-22 00:07:49 +0530

    all

---
+++ Backend/index.js
@@ -0,0 +1,271 @@
+const express = require("express");
+const path = require("path");
+
+const methodOverride=require("method-override")
+const app = express();
+const mysql=require("mysql2");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "shrinath1814",
+    database: "sunil"
+});
+
+connection.connect((err) => {
+    if(err){
+        console.log("Database Error :", err);
+    } else {
+        console.log("MySQL Connected Successfully");
+    }
+});
+
+
+app.set("view engine", "ejs");
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); 
+
+// ejs template setup
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+
+
+//style.css 
+app.use(express.static(path.join(__dirname, "Public")));
+
+//dashboad
+app.get("/", (req, res) => {
+
+    let q = `
+        SELECT
+            loan.Name,
+            loan.Loan,
+            loan.Interest,
+            loan.Remaining,
+            IFNULL(SUM(interest_history.interest_amount), 0) AS total_interest
+        FROM loan
+        LEFT JOIN interest_history
+        ON loan.Name = interest_history.customer_name
+        GROUP BY
+            loan.Name,
+            loan.Loan,
+            loan.Interest,
+            loan.Remaining
+    `;
+
+    connection.query(q, (err, results) => {
+
+        if(err){
+            console.log(err);
+            return res.status(500).send("Database Error");
+        }
+
+        res.render("dashboard", {
+            loans: results
+        });
+
+    });
+
+});
+
+//add customer
+app.get("/add-customer", (req, res) => {
+    res.render("addcustomer");
+});
+
+app.get("/dashboad",(req,res)=>{
+   let q= "SELECT * FROM loan";
+   connection.query(q,(err,results)=>{
+       if(err){
+           console.log("Error fetching loan data:", err);
+           res.status(500).send("Error fetching loan data");
+
+       } else {
+           res.render("dashboard", { loans: results });
+       }
+   });
+});
+app.post("/customer/new", (req, res) => {
+
+    let { name, loan, interest } = req.body;
+
+    let q = `
+        INSERT INTO loan
+        (Name, Loan, Interest, Remaining)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    connection.query(
+        q,
+        [name, loan, interest, loan],
+        (err, result) => {
+
+            if(err){
+                console.log(err);
+                return res.send("Database Error");
+            }
+
+            res.redirect("/");
+        }
+    );
+});
+
+app.get("/interest/:name", (req, res) => {
+
+    let name = req.params.name;
+
+    res.render("interest", { name });
+
+});
+
+app.post("/interest/add", (req, res) => {
+
+    let {
+        customer_name,
+        interest_amount,
+        interest_date
+    } = req.body;
+
+    console.log(req.body);
+
+    let q = `
+        INSERT INTO interest_history
+        (customer_name, interest_amount, interest_date)
+        VALUES (?, ?, ?)
+    `;
+
+    connection.query(
+        q,
+        [
+            customer_name,
+            interest_amount,
+            interest_date
+        ],
+        (err, result) => {
+
+            if(err){
+                console.log(err);
+                return res.send(err.sqlMessage);
+            }
+
+            console.log("Interest Added Successfully");
+
+            res.redirect("/");
+        }
+    );
+});
+
+
+app.get("/history/:name", (req, res) => {
+
+    let name = req.params.name;
+
+    let loanQuery = `
+        SELECT *
+        FROM loan
+        WHERE Name = ?
+        LIMIT 1
+    `;
+
+    let historyQuery = `
+        SELECT *
+        FROM interest_history
+        WHERE customer_name = ?
+        ORDER BY interest_date ASC
+    `;
+
+    connection.query(loanQuery, [name], (err, loanResult) => {
+
+        if(err){
+            console.log(err);
+            return res.send("Database Error");
+        }
+
+        if(loanResult.length === 0){
+            return res.send("Customer Not Found");
+        }
+
+        connection.query(historyQuery, [name], (err, historyResult) => {
+
+            if(err){
+                console.log(err);
+                return res.send("Database Error");
+            }
+
+            res.render("history", {
+                customer: loanResult[0],
+                history: historyResult
+            });
+
+        });
+
+    });
+
+});
+app.post("/return/add", (req, res) => {
+
+    let {
+        customer_name,
+        return_amount,
+        return_date
+    } = req.body;
+
+    let insertQuery = `
+        INSERT INTO return_history
+        (customer_name, return_amount, return_date)
+        VALUES (?, ?, ?)
+    `;
+
+    connection.query(
+        insertQuery,
+        [customer_name, return_amount, return_date],
+        (err, result) => {
+
+            if(err){
+                console.log(err);
+                return res.send("Database Error");
+            }
+
+            let updateQuery = `
+                UPDATE loan
+                SET Remaining = Remaining - ?
+                WHERE Name = ?
+            `;
+
+            connection.query(
+                updateQuery,
+                [return_amount, customer_name],
+                (err, result) => {
+
+                    if(err){
+                        console.log(err);
+                        return res.send("Database Error");
+                    }
+
+                    res.redirect("/");
+                }
+            );
+
+        }
+    );
+
+});
+
+
+app.get("/return/:name", (req, res) => {
+
+    let name = req.params.name;
+
+    res.render("return", {
+        name: name
+    });
+
+});
+
+
+app.listen(5000, () => {
+    console.log("Server Running On Port 5000");
+});
+
